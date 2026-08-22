54543cd2bbb012c3632f3d4b916a3fec8ede0d02
jamesCarl1204 <jamesmagsipoc887@gmail.com>
2026-08-22 20:40:12 +0800

    initial commit calculator projects

---
+++ projects/calculator/script.js
@@ -0,0 +1,26 @@
+document.getElementById("num1").innerhtml = 1;
+document.getElementById("num2").innerhtml = 1;
+document.getElementById("num3").innerhtml = 1;
+document.getElementById("num4").innerhtml = 1;
+document.getElementById("num5").innerhtml = 1;
+document.getElementById("num6").innerhtml = 1;
+document.getElementById("num7").innerhtml = 1;
+document.getElementById("num8").innerhtml = 1;
+document.getElementById("num9").innerhtml = 1;
+document.getElementById("num10").innerhtml = 1;
+document.getElementById("num1").innerhtml = 1;
+
+
+let n = []
+
+function num(number) {
+     
+     document.getElementById("input").value = number
+     n.push(number)
+     
+}
+
+function add({number}) {
+    
+
+}
